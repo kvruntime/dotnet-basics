f117ce42a6b2ac3e3ca7e9a0d6e5e5dc40176a7e
kvruntime <kvruntime@gmail.com>
2023-10-06 08:13:05 +0000

    feat(csle):enhance console application

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter your name: ");
+var name = Console.ReadLine();
+if (String.IsNullOrWhiteSpace(name))
+{
+  Console.WriteLine("No name provided, program exit...");
+  Environment.Exit(0);
+}
+
+Console.WriteLine($"Hello {name} at {DateTime.UtcNow:d} on {DateTime.Now:t}");
+Console.WriteLine($"{Environment.NewLine}Press any key...");
+
+Console.ReadKey(true);
