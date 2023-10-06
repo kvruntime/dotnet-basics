6d73218336eceaa91bf22814c8a04440bd042c65
kvruntime <kvruntime@gmail.com>
2023-10-06 09:43:14 +0000

    feat(*): complete tutorials

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,18 @@
-﻿using StringClasslib.Utilities;
+﻿using System.Globalization;
+using StringClasslib.Utilities;
 
 
 
 
 Console.Write("Enter your name: ");
 var name = Console.ReadLine();
-if (String.IsNullOrWhiteSpace(name) || !StringOperations.StartsWithUpper(name))
+if (String.IsNullOrWhiteSpace(name))
 {
   Console.WriteLine("Invalid name provided, program exit...");
   Environment.Exit(0);
 }
+
+name = StringOperations.StartsWithUpper(name) ? name : CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
 
 
 Console.WriteLine($"Hello {name} at {DateTime.UtcNow:d} on {DateTime.Now:t}");
